f7d26c62eef71248f97a30a80a2f3e3db54e0ea1
Lois Choi and Michael Woldemedihin <lois@>
2017-08-16 11:53:15 -0700

    add MySqlConnection to Database.cs

--- BestRestaurant/Model/Cuisine.cs
+++ BestRestaurant/Model/Cuisine.cs
@@ -5,6 +5,51 @@ namespace BestRestaurant.Models
 {
   public class Cuisine
   {
+    private int _id;
+    private string _cuisineName;
+
+    public Cuisine(string cuisineName, int id = 0)
+    {
+      _id = id;
+      _cuisineName = cuisineName;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetCuisineName()
+    {
+      return _cuisineName;
+    }
+
+    public void Save()
+    {
+      //Save method after lunch
+    }
+
+    public static List<Cuisine> GetAllCuisines()
+    {
+      List<Cuisine> allCuisine = new List<Cuisine> {};
+
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM cuisines;";
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+      while(rdr.Read())
+      {
+        int cuisine_id = rdr.GetInt32(0);
+        string cuisine_name = rdr.GetString(1);
+        Cuisine newCuisine = new Cuisine(cuisine_name, cuisine_id);
+        allCuisine.Add(newCuisine);
+      }
+
+      return allCuisine;
+    }
+
 
   }
 }
--- BestRestaurant/Startup.cs
+++ BestRestaurant/Startup.cs
@@ -37,9 +37,4 @@ namespace BestRestaurant
         });
     }
   }
-
-  public static class DBConfiguration
-    {
-        public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo;";
-    }
 }
